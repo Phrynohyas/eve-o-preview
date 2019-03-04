9e5738b74a308a1ca996e0491adaf15d48bfa6dd
Anton Kasyanov <antonk@remobjects.com>
2019-03-04 21:50:25 +0200

    143: Add a simple nag message box for Beta builds

--- Eve-O-Preview/Program.cs
+++ Eve-O-Preview/Program.cs
@@ -17,12 +17,20 @@ namespace EveOPreview
 		[STAThread]
 		static void Main()
 		{
+#if DEBUG
+			var expirationDate = new DateTime(2019, 5, 1);
+			if (DateTime.Today >= expirationDate)
+			{
+				MessageBox.Show(@"This Beta version is expired. Please download a new build at https://github.com/Phrynohyas/eve-o-preview/releases", @"EVE-O Preview", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			}
+#endif
+
 			// The very usual Mutex-based single-instance screening
 			// 'token' variable is used to store reference to the instance Mutex
 			// during the app lifetime
 			object token = Program.GetInstanceToken();
 
-			// If it was not possible to aquire the app token then another app instance is already running
+			// If it was not possible to acquire the app token then another app instance is already running
 			// Nothing to do here
 			if (token == null)
 			{
